281c321a8dc7a967eb0f33946c1b118aace98593
vitalyvitmens <otm_@tut.by>
2024-04-08 15:36:19 +0300

    test(src\unit.js): добавляет unit test

---
+++ src/unit.js
@@ -0,0 +1,13 @@
+import { fireEvent, render } from '@testing-library/react'
+import Button from './Button'
+
+describe('Button', () => {
+	it('вызывает onClick при клике', () => {
+		const onClick = jest.fn()
+		const view = render(<Button onClick={onClick}>Click me</Button>)
+		const button = view.getByText('Click me')
+
+		fireEvent.click(button)
+		expect(onClick).toHaveBeenCalled(1)
+	})
+})
